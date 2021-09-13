c2adceb56d421af27377af35ab579b4b746228d6
Phuoc <tdphuoc77@gmail.com>
2021-09-13 21:02:21 +0700

    first commit

---
+++ src/main/java/com/kttkpm_13_09/Kttkpm1309Application.java
@@ -0,0 +1,13 @@
+package com.kttkpm_13_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kttkpm1309Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kttkpm1309Application.class, args);
+    }
+
+}
